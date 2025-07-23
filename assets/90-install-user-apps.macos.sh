dd600cb064fad7b0f7e55e29055ebb756485180d
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2025-07-23 10:32:24 +0100

    Add user apps

---
+++ assets/90-install-user-apps.macos.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+# Install base packages on macOS
+#
+# Usage:
+#   $ source ./commons.sh
+#   $ ./20-install-base-packages.macos.sh
+#
+# Options:
+#   REINSTALL=true  # Attempts to reinstall the packages, default is `false`
+#   VERBOSE=true    # Show all the executed commands, default is `false`
+
+# ==============================================================================
+
+function main {
+
+  # Customise brew execution
+  export HOMEBREW_NO_AUTO_UPDATE=1
+  is-arg-true "$REINSTALL" && export install="reinstall --force" || export install="install"
+
+  install
+}
+
+function install {
+
+  brew $install \
+    enpass \
+    notion \
+    spotify \
+    ||:
+}
+
+# ==============================================================================
+
+is-arg-true "$VERBOSE" > /dev/null 2>&1 && set -x
+
+main $*
+
+exit 0
